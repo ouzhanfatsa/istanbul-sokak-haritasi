637204de234de70ddafe6be449906535b14511c8
ouzhanfatsa <ouzhanfatsa@users.noreply.github.com>
2026-06-20 00:43:03 +0300

    İstanbul Sokak Genişlik Haritası - PWA

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'istanbul-sokak-v1';
+const urlsToCache = [
+  '/istanbul-sokak-haritasi.html',
+  '/manifest.json',
+  '/icon.svg',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names => Promise.all(
+      names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request).catch(() => new Response('Çevrimdışı', { status: 503 })))
+  );
+});
